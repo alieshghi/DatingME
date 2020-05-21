e11f14c3f9fe7b394c9f1101951470523ae24311
alieshghi <a.eshghi94@gmail.com>
2020-05-22 01:32:02 +0430

    cloudinary starts

---
+++ TodoApi/helper/CloudinarySetting.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.helper
+{
+    public class CloudinarySetting
+    {
+        public string Cloudname { get; set; }
+        public string APIKey { get; set; }
+        public string APISecret { get; set; }
+    }
+}
